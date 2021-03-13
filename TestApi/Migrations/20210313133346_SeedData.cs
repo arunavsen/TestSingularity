57ca7a8e87fa973a6ad3c0678047759cc5536a81
Arunav <arunav@vonome.com>
2021-03-13 19:36:02 +0600

    Sedding data

---
+++ TestApi/Migrations/20210313133346_SeedData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestApi.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Name", "Price" },
+                values: new object[] { 1, "Product 1", 100.0 });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Name", "Price" },
+                values: new object[] { 2, "Product 2", 200.0 });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Name", "Price" },
+                values: new object[] { 3, "Product 3", 300.0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
--- TestApi/Models/ApplicationDbContext.cs
+++ TestApi/Models/ApplicationDbContext.cs
@@ -14,5 +14,28 @@ namespace TestApi.Models
         }
 
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1, Name = "Product 1", Price = 100
+                },
+            new Product
+                {
+                    Id = 2,
+                    Name = "Product 2",
+                    Price = 200
+                },
+                new Product
+                {
+                    Id = 3,
+                    Name = "Product 3",
+                    Price = 300
+                }
+
+            );
+        }
     }
 }
