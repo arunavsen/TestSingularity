2f398328f375c3186d77ed59198e5216bd4bdfba
Arunav <arunav@vonome.com>
2021-03-14 20:13:05 +0600

    Add Swagger

--- TestApi/Startup.cs
+++ TestApi/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using TestApi.Models;
 
 namespace TestApi
@@ -61,6 +62,46 @@ namespace TestApi
                 };
             });
 
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("TestSingularity", new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Title = "Test Singularity",
+                    Version = "1"
+                });
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description =
+                        "JWT Authorization header using the Bearer scheme. \r\n\r\n " +
+                        "Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
+                        "Example: \"Bearer 12345abcdef\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -73,7 +114,18 @@ namespace TestApi
 
             app.UseHttpsRedirection();
 
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/TestSingularity/swagger.json", "Test Singularity");
+                options.RoutePrefix = "";
+
+            });
+
             app.UseCors("CorsPolicy");
+
+            app.UseAuthentication();
 
             app.UseRouting();
 
